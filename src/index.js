ebda6ac2f6486a6af10c5361e03a8796d8a43395
ankit-95 <ankitgv.kvtagore@gmail.com>
2021-05-30 12:47:21 +0530

    Change : Particles.js added in Background

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import Card from "./Cards";
 import "./index.css";
 import Sdata from "./Sdata";
+import Particles from "react-particles-js";
+import particlesConfig from "./particlesConfig";
 
 let ncard = (val) => {
   console.log(val);
@@ -19,8 +21,25 @@ let ncard = (val) => {
 
 ReactDOM.render(
   <>
-    <h1 className="heading-style">List of top 5 Netflix in 2021</h1>
-    {Sdata.map(ncard)};
+    <div className="App" style={{ position: "relative", overflow: "hidden" }}>
+      <div style={{ position: "relative" }}>
+        <Particles
+          height="100%"
+          width="100%"
+          params={particlesConfig}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        />
+        <h1 className="heading-style">List of top 5 Netflix in 2021</h1>
+        {Sdata.map(ncard)};
+      </div>
+    </div>
+    ;
   </>,
   document.getElementById("root")
 );
