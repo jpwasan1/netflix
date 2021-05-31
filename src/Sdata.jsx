a597b279cf64f1e968ebbc35bf7728203c764d3b
Jaspreet Singhwasan <jpwasan1@gmail.com>
2021-05-31 15:34:40 +0530

    added fb link

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -4,42 +4,42 @@ const Sdata =
         imgsrc: "https://res.cloudinary.com/stanza-living/image/upload/f_auto,q_auto,w_355/v1585230160/Website/CMS-Uploads/j6nmhp7foszuo6leeoqq.jpg",
         title: " GOOD FEELING HOSTEL",
         sname: "DARK",
-        link: "https://fruitguys.com/"
+        link: "https://m.facebook.com/jitender.wasan.90/about?lst=100005219057795%3A100055231015060%3A1622455262"
     },
     {
         id: 2,
         imgsrc: "https://www.stayforyou.com/wp-content/uploads/2019/06/Untitled-design-660x600.jpg",
         title: " GOOD FEELING HOSTEL",
         sname: "DARK",
-        link: "https://fruitguys.com/"
+        link: "https://m.facebook.com/jitender.wasan.90/about?lst=100005219057795%3A100055231015060%3A1622455262"
     },
     {
         id: 3,
         imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbrinYuvB1mfvUrBtAZh0o-yIgPC3qAI3xGQ&usqp=CAU",
         title: " GOOD FEELING HOSTEL",
         sname: "DARK",
-        link: "https://fruitguys.com/"
+        link: "https://m.facebook.com/jitender.wasan.90/about?lst=100005219057795%3A100055231015060%3A1622455262"
     },
     {
         id: 4,
         imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSka3ejdaMDPwfwIow7fee0uPtyXReoNBolfA&usqp=CAU",
         title: " GOOD FEELING HOSTEL",
         sname: "DARK",
-        link: "https://fruitguys.com/"
+        link: "https://m.facebook.com/jitender.wasan.90/about?lst=100005219057795%3A100055231015060%3A1622455262"
     },
     {
         id: 5,
         imgsrc: "https://s3.ap-south-1.amazonaws.com/www.cimg.in/images/2011/06/04/07/9957617_13450990741_large.jpg",
         title: " GOOD FEELING HOSTEL",
         sname: "DARK",
-        link: "https://fruitguys.com/"
+        link: "https://m.facebook.com/jitender.wasan.90/about?lst=100005219057795%3A100055231015060%3A1622455262"
     },
     {
         id: 6,
         imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWgQua8qWFGjr246PgMRF5x6d8RtvJ-bd7sg&usqp=CAU",
         title: " GOOD FEELING HOSTEL",
         sname: "DARK",
-        link: "https://fruitguys.com/"
+        link: "https://m.facebook.com/jitender.wasan.90/about?lst=100005219057795%3A100055231015060%3A1622455262"
     }
     ];
 
